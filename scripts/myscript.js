22585fe60df59a5dad3f33069d3460f1cecc5dee
Drakuras <74796092+Drakuras@users.noreply.github.com>
2021-07-03 14:20:35 +0100

    Added animations and a logo

--- scripts/myscript.js
+++ scripts/myscript.js
@@ -2,4 +2,12 @@
 window.addEventListener("scroll",function(){
     let nav = this.document.querySelector("nav");
     nav.classList.toggle("sticky",window.scrollY > 0)
-})
+});
+
+window.onscroll = function () {
+    scrollRotate();
+};
+function scrollRotate() {
+    let image = document.getElementsByTagName("img")
+    image[1].style.transform = "rotate(" + window.pageYOffset/3 + "deg)";
+}
